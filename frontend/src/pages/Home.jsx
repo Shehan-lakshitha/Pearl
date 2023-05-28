031ed425c94de1b2c61961a2dde184c40244ba68
Shehan-Lakshitha <shehanlaksh@gmail.com>
2023-05-28 09:57:19 +0530

    Edited hero

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -17,13 +17,24 @@ const Home = () => {
           <Row>
             <Col lg='6'>
               <div className="hero_Content">
-                {/* <div className="hero_subtitle d-flex align-items-center">
-                  <Subtitle subtitle={'Know Before You Go'}/>
-                  <img src={worldImg} alt=""/>
-                </div> */}
                 <h1>Welecome <br/> to Sri Lanka</h1>
                 <p>Discover the beauty of Sri Lanka - pristine beaches, lush tea plantations, ancient temples, and vibrant markets. Experience warm hospitality, delicious cuisine, and create lifelong memories. Explore Sri Lanka's wonders and let its enchanting landscapes captivate you. Start your journey today!</p>
+              </div>
+            </Col>
 
+            <Col lg='2'>
+              <div className="hero_img-box">
+                <img src={heroImg} alt=''/>
+              </div>
+            </Col>
+            <Col lg='2'>
+              <div className="hero_img-box mt-4">
+                <video src={heroVideo} alt='' controls />
+              </div>
+            </Col>
+            <Col lg='2'>
+              <div className="hero_img-box mt-5">
+                <img src={heroImg02} alt=''/>
               </div>
             </Col>
           </Row>
