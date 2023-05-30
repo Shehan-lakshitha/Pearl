1edb73a7c8175ca8979b510af33a46ac74aacb18
Shehan-Lakshitha <shehanlaksh@gmail.com>
2023-05-30 10:29:06 +0530

    Added services card

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,46 +1,55 @@
-import React from 'react';
-import '../styles/home.css';
+import React from "react";
+import "../styles/home.css";
 
-import { Container, Row,Col } from 'reactstrap';
-import heroImg from '../assets/images/hero-img01.jpg';
-import heroImg02 from '../assets/images/hero-img02.jpg';
-import heroVideo from '../assets/images/hero-video.mp4';
-import worldImg from '../assets/images/world.png';
-import Subtitle from './../shared/Subtitle';
+import { Container, Row, Col } from "reactstrap";
+import heroImg from "../assets/images/hero-img01.jpg";
+import heroImg02 from "../assets/images/hero-img02.jpg";
+import heroVideo from "../assets/images/hero-video.mp4";
+import worldImg from "../assets/images/world.png";
+import Subtitle from "./../shared/Subtitle";
 
-import SearchBar from '../shared/SearchBar';
+import SearchBar from "../shared/SearchBar";
+import ServiceList from "../services/ServiceList";
 
 const Home = () => {
   return (
     <>
-    {/* hero section start */}
+      {/* hero section start */}
       <section className="hero">
         <Container>
           <Row>
-            <Col lg='6'>
+            <Col lg="6">
               <div className="hero_Content">
-                <h1>Welecome <br/> to Sri Lanka</h1>
-                <p>Discover the beauty of Sri Lanka - pristine beaches, lush tea plantations, ancient temples, and vibrant markets. Experience warm hospitality, delicious cuisine, and create lifelong memories. Explore Sri Lanka's wonders and let its enchanting landscapes captivate you. Start your journey today!</p>
+                <h1>
+                  Welecome <br /> to Sri Lanka
+                </h1>
+                <p>
+                  Discover the beauty of Sri Lanka - pristine beaches, lush tea
+                  plantations, ancient temples, and vibrant markets. Experience
+                  warm hospitality, delicious cuisine, and create lifelong
+                  memories. Explore Sri Lanka's wonders and let its enchanting
+                  landscapes captivate you. Start your journey today!
+                </p>
               </div>
             </Col>
 
-            <Col lg='2'>
+            <Col lg="2">
               <div className="hero_img-box">
-                <img src={heroImg} alt=''/>
+                <img src={heroImg} alt="" />
               </div>
             </Col>
-            <Col lg='2'>
+            <Col lg="2">
               <div className="hero_img-box mt-4">
-                <video src={heroVideo} alt='' controls />
+                <video src={heroVideo} alt="" controls />
               </div>
             </Col>
-            <Col lg='2'>
+            <Col lg="2">
               <div className="hero_img-box mt-5">
-                <img src={heroImg02} alt=''/>
+                <img src={heroImg02} alt="" />
               </div>
             </Col>
 
-            <SearchBar/>
+            <SearchBar />
           </Row>
         </Container>
       </section>
@@ -49,15 +58,16 @@ const Home = () => {
       <section>
         <Container>
           <Row>
-            <Col lg='3'>
+            <Col lg="3">
               <h5 className="services_subtitle">What we serve</h5>
               <h2 className="service_title">We offer our best services</h2>
             </Col>
+            <ServiceList />
           </Row>
         </Container>
       </section>
     </>
   );
-}
+};
 
-export default Home
+export default Home;
