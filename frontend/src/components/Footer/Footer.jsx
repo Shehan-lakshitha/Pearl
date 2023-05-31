7f8042d9e581d21c20a970c768812be78461c515
Shehan-Lakshitha <shehanlaksh@gmail.com>
2023-05-31 22:29:55 +0530

    Added footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,71 +1,146 @@
-import React from 'react'
-import './footer.css';
+import React from "react";
+import "./footer.css";
 
-import { Container, Row, Col, ListGroup, ListGroupItem } from 'reactstrap';
+import { Container, Row, Col, ListGroup, ListGroupItem } from "reactstrap";
 
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import logo from "../../assets/images/logo.png";
 
-const quick_links =[
+const quick_links = [
   {
-    path:'/home',
-    display:'Home'
+    path: "/home",
+    display: "Home",
   },
   {
-    path:'/about',
-    display:'About'
+    path: "/about",
+    display: "About",
   },
   {
-    path:'/tours',
-    display:'Tours'
+    path: "/tours",
+    display: "Tours",
   },
 ];
 
-const quick_links2 =[
+const quick_links2 = [
   {
-    path:'/gallery',
-    display:'Gallery'
+    path: "/gallery",
+    display: "Gallery",
   },
   {
-    path:'/login',
-    display:'Login'
+    path: "/login",
+    display: "Login",
   },
   {
-    path:'/register',
-    display:'Register'
+    path: "/register",
+    display: "Register",
   },
-]
+];
 
 const Footer = () => {
+
+  const year = new Date().getFullYear();
+
   return (
-  <footer className="footer">
-    <Container>
-      <Row>
-        <Col lg='3'>
-          <div className="logo">
-            <img src={logo} alt=''/>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, enim.</p>
+    <footer className="footer">
+      <Container>
+        <Row>
+          <Col lg="3">
+            <div className="logo">
+              <img src={logo} alt="" />
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Commodi, enim.
+              </p>
 
-            <div className="social_links" d-flex align-items-center gap-4>
-              <span>
-                <Link to='#'><i class="ri-youtube-fill"></i></Link>
-              </span>
-              <span>
-                <Link to='#'><i class="ri-github-fill"></i></Link>
-              </span>
-              <span>
-                <Link to='#'><i class="ri-facebook-circle-fill"></i></Link>
-              </span>
-              <span>
-                <Link to='#'><i class="ri-instagram-fill"></i></Link>
-              </span>
+              <div className="social_links d-flex align-items-center gap-4">
+                <span>
+                  <Link to="#">
+                    <i class="ri-youtube-fill"></i>
+                  </Link>
+                </span>
+                <span>
+                  <Link to="#">
+                    <i class="ri-github-fill"></i>
+                  </Link>
+                </span>
+                <span>
+                  <Link to="#">
+                    <i class="ri-facebook-circle-fill"></i>
+                  </Link>
+                </span>
+                <span>
+                  <Link to="#">
+                    <i class="ri-instagram-fill"></i>
+                  </Link>
+                </span>
+              </div>
             </div>
-          </div>
-        </Col>
-      </Row>
-    </Container>
-  </footer>
-  );
-}
+          </Col>
+          <Col lg="3">
+            <h5 className="footer_link-title">Discover</h5>
 
-export default Footer
+            <ListGroup className="footer_quick-links">
+              {quick_links.map((item, index) => (
+                <ListGroupItem key={index} className="ps-0 border-0">
+                  <Link to={item.path}>{item.display}</Link>
+                </ListGroupItem>
+              ))}
+            </ListGroup>
+          </Col>
+          <Col lg="3">
+            <h5 className="footer_link-title">Quick Links</h5>
+
+            <ListGroup className="footer_quick-links">
+              {quick_links2.map((item, index) => (
+                <ListGroupItem key={index} className="ps-0 border-0">
+                  <Link to={item.path}>{item.display}</Link>
+                </ListGroupItem>
+              ))}
+            </ListGroup>
+          </Col>
+          <Col lg="3">
+            <h5 className="footer_link-title">Contact</h5>
+
+            <ListGroup className="footer_quick-links">
+              <ListGroupItem className="ps-0 border-0 d-flex align-items-center gap-3">
+                <h6 className="mb=0 d-flex align-items-center gap-2">
+                  <span>
+                    <i class="ri-map-pin-fill"></i>
+                  </span>
+                  Address:
+                </h6>
+
+                <p className="mb-0">Belihuloya, Balangoda</p>
+              </ListGroupItem>
+              <ListGroupItem className="ps-0 border-0 d-flex align-items-center gap-3">
+                <h6 className="mb=0 d-flex align-items-center gap-2">
+                  <span>
+                  <i class="ri-mail-fill"></i>
+                  </span>
+                  Email:
+                </h6>
+
+                <p className="mb-0">info@pearl.com</p>
+              </ListGroupItem>
+              <ListGroupItem className="ps-0 border-0 d-flex align-items-center gap-3">
+                <h6 className="mb=0 d-flex align-items-center gap-2">
+                  <span>
+                  <i class="ri-phone-fill"></i>
+                  </span>
+                  Phone:
+                </h6>
+
+                <p className="mb-0">+941122565</p>
+              </ListGroupItem>
+            </ListGroup>
+          </Col>
+          <Col lg='12' className="text-center pt-5">
+            <p className="copyright">Copyrights {year} All rights reserved</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
