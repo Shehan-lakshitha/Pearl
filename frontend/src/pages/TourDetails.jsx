5283474174b822f07dde9a738d94d90230bdbc43
Shehan Lakshitha <shehanlaksh@gmail.com>
2023-06-02 22:20:50 +0530

    added tourdetails

--- frontend/src/pages/TourDetails.jsx
+++ frontend/src/pages/TourDetails.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import '../styles/tour-details.css';
+import { Container, Row, Col, Form, ListGroup } from 'reactstrap';
+import { useParams } from 'react-router-dom';
 
 const TourDetails = () => {
   return (
