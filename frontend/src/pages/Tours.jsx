e9e2f6596b87d2f272fbb392827f1083d397edfc
Shehan Lakshitha <shehanlaksh@gmail.com>
2023-06-02 20:20:33 +0530

    Added tour section

--- frontend/src/pages/Tours.jsx
+++ frontend/src/pages/Tours.jsx
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import CommonSection from "../shared/CommonSection";
 
-import '../styles/tour.css';
-import tourData from '../assets/data/tours';
+import "../styles/tour.css";
+import tourData from "../assets/data/tours";
 import TourCard from "../shared/TourCard";
 import SearchBar from "../shared/SearchBar";
 import Newsletter from "../shared/Newsletter";
 
-import { Container } from "reactstrap";
-import { Row } from "reactstrap";
+import { Col, Container, Row } from "reactstrap";
 
 const Tours = () => {
+  const [pageCount, setPageCount] = useState(0);
+  const [page, setPage] = useState(0);
+
+  useEffect(() => {
+    const pages = Math.ceil(5 / 4); //TODO Need to add count in backend
+    setPageCount(pages);
+  }, [page]);
+
   return (
     <>
       <CommonSection title={"All tours"} />
@@ -21,6 +28,33 @@ const Tours = () => {
           </Row>
         </Container>
       </section>
+      <section className="pt-0">
+        <Container>
+          <Row>
+            {tourData?.map((tour) => (
+              <Col lg="3" className="mb-4">
+                {" "}
+                <TourCard tour={tour} key={tour.id} />{" "}
+              </Col>
+            ))}
+
+            <Col lg="12">
+              <div className="pagination d-flex align-items-center justify-content-center mt-4 gap-3">
+                {[...Array(pageCount).keys()].map((number) => (
+                  <span
+                    key={number}
+                    onClick={() => setPage(number)}
+                    className={page === number ? "active_page" : ""}
+                  >
+                    {number + 1}
+                  </span>
+                ))}
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+      <Newsletter />
     </>
   );
 };
