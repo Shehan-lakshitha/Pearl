2451d83914a331fb4647a1ae7f26cacbaf03a85d
Shehan-Lakshitha <shehanlaksh@gmail.com>
2023-06-02 12:16:00 +0530

    Added login section

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import React ,{useState} from "react";
+import React, { useState } from "react";
 
 import { Container, Row, Col, Form, FormGroup, Button } from "reactstrap";
 import { Link } from "react-router-dom";
@@ -8,55 +8,68 @@ import loginImg from "../assets/images/login.png";
 import userIcon from "../assets/images/user.png";
 
 const Login = () => {
-
   const [credentials, setCredentials] = useState({
     email: undefined,
-    password: undefined
+    password: undefined,
   });
 
   const handleChange = (e) => {
-    setCredentials(prev => ({...prev, [e.target.id]: e.target.value}));
+    setCredentials((prev) => ({ ...prev, [e.target.id]: e.target.value }));
   };
 
-  const handleSubmit = (e) => {
+  const handleClick = (e) => {
     e.preventDefault();
-  }
+  };
 
   return (
-  <section>
-    <Container>
-      <Row>
-        <Col lg='8' className="m-auto">
-          <div className="login_container d-flex justify-content-between">
-            <div className="login_img">
-              <img src={loginImg} alt="" />
-            </div>
-
-            <div className="login_form">
-              <div className="user">
-                <img src={userIcon} alt="" />
+    <section>
+      <Container>
+        <Row>
+          <Col lg="8" className="m-auto">
+            <div className="login_container d-flex justify-content-between">
+              <div className="login_img">
+                <img src={loginImg} alt="" />
               </div>
-              <h2>Login</h2>
 
-              <Form>
-                <FormGroup>
-                  <input type="text" placeholder="Email" required id="email" onChange={handleChange} />
-                </FormGroup>
-                <FormGroup>
-                  <input type="password" placeholder="Password" required id="password" onChange={handleChange} />
-                </FormGroup>
-                <Button className="btn secondary__btn auth__btn" type="submit">Login</Button>
-              </Form>
-              <p>Don't have an account?  <Link to='/register'></Link></p>
+              <div className="login_form">
+                <div className="user">
+                  <img src={userIcon} alt="" />
+                </div>
+                <h2>Login</h2>
+
+                <Form onClick={handleClick}> 
+                  <FormGroup>
+                    <input
+                      type="text"
+                      placeholder="Email"
+                      required
+                      id="email"
+                      onChange={handleChange}
+                    />
+                  </FormGroup>
+                  <FormGroup>
+                    <input
+                      type="password"
+                      placeholder="Password"
+                      required
+                      id="password"
+                      onChange={handleChange}
+                    />
+                  </FormGroup>
+                  <Button className="btn_login auth__btn" type="submit">
+                    Login
+                  </Button>
+                </Form>
+                <p>
+                  Don't have an account? <Link to="/register"> Sign Up</Link>
+                </p>
+              </div>
             </div>
-          </div>
-
-
-        </Col>
-      </Row>
-    </Container>
-  </section>
-  )
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
 };
 
 export default Login;
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,11 +1,76 @@
-import React from 'react'
+import React, { useState } from "react";
+
+import { Container, Row, Col, Form, FormGroup, Button } from "reactstrap";
+import { Link } from "react-router-dom";
+import "../styles/login.css";
+
+import loginImg from "../assets/images/login.png";
+import userIcon from "../assets/images/user.png";
 
 const Register = () => {
-  return (
-    <div>
-      Register
-    </div>
-  )
-}
+  const [credentials, setCredentials] = useState({
+    email: undefined,
+    password: undefined,
+  });
 
-export default Register
+  const handleChange = (e) => {
+    setCredentials((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+  };
+
+  const handleClick = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <section>
+      <Container>
+        <Row>
+          <Col lg="8" className="m-auto">
+            <div className="login_container d-flex justify-content-between">
+              <div className="login_img">
+                <img src={loginImg} alt="" />
+              </div>
+
+              <div className="login_form">
+                <div className="user">
+                  <img src={userIcon} alt="" />
+                </div>
+                <h2>Login</h2>
+
+                <Form onClick={handleClick}> 
+                  <FormGroup>
+                    <input
+                      type="text"
+                      placeholder="Email"
+                      required
+                      id="email"
+                      onChange={handleChange}
+                    />
+                  </FormGroup>
+                  <FormGroup>
+                    <input
+                      type="password"
+                      placeholder="Password"
+                      required
+                      id="password"
+                      onChange={handleChange}
+                    />
+                  </FormGroup>
+                  <Button className="btn_login auth__btn" type="submit">
+                    Login
+                  </Button>
+                </Form>
+                <p>
+                  Already have an account <Link to="/register"> Login</Link>
+                </p>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default Register;
+
