29691d2e5c8b80c3cf518749b66f51bf59a54aa4
Shehan Lakshitha <shehanlaksh@gmail.com>
2023-06-02 18:49:22 +0530

    Added search bar in tour page

--- frontend/src/pages/Tours.jsx
+++ frontend/src/pages/Tours.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 import CommonSection from "../shared/CommonSection";
 
+import '../styles/tour.css';
+import tourData from '../assets/data/tours';
+import TourCard from "../shared/TourCard";
+import SearchBar from "../shared/SearchBar";
+import Newsletter from "../shared/Newsletter";
+
+import { Container } from "reactstrap";
+import { Row } from "reactstrap";
+
 const Tours = () => {
   return (
     <>
       <CommonSection title={"All tours"} />
+      <section>
+        <Container>
+          <Row>
+            <SearchBar />
+          </Row>
+        </Container>
+      </section>
     </>
   );
 };
