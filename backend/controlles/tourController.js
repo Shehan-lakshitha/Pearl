05056c35c273ec4d63cc13891d09a91b05527657
Dilini Gnanarathne <shashikaladilini11@gmail.com>
2023-06-13 23:56:38 +0530

    Update , delete , getSingle & getAll function success

--- backend/controlles/tourController.js
+++ backend/controlles/tourController.js
@@ -18,36 +18,86 @@ export const createTour = async(req,res)=>{
     }
 }
 
-/*
-//  updateTour                                                            
+//updateTour                                                            
 export const updateTour=async(req,res)=>{
+   const id = req.params.id
     try {
-        
+        const updateTour= await Tour.findByIdAndUpdate(id,{
+            $set:req.body
+        },{new:true})
+        res
+        .status(200)
+        .json({
+            success:true,
+            message:"Successfully updated",
+            data: updateTour,
+        });
+
     } catch (error) {
-        
+        res
+        .status(500)
+        .json({success:false,message:"Failed to update. Try again"});
     }
 }
+
 //deleteTour
 export const deleteTour=async(req,res)=>{
+    
+    const id = req.params.id
     try {
-        
+         await Tour.findByIdAndDelete(id)
+        res
+        .status(200)
+        .json({
+            success:true,
+            message:"Successfully deleted",
+        });
+
     } catch (error) {
-        
+        res
+        .status(500)
+        .json({success:false,message:"Failed to delete. Try again"});
     }
 }
+
 //getSingleTour
 export const getSingleTour=async(req,res)=>{
+    const id = req.params.id
     try {
-        
+        const tour= await Tour.findById(id)
+        res
+        .status(200)
+        .json({
+            success:true,
+            message:"Successfully deleted",
+            data:tour,
+        });
+
     } catch (error) {
-        
+        res
+        .status(404)
+        .json({success:false,message:"Not found"});
     }
 }
+
 //getAllTour
 export const getAllTour=async(req,res)=>{
+    //for pagination
+    const page=parseInt(req.query.page);
+    console.log(page);
     try {
-        
+        const tours= await Tour.find({});
+        res
+        .status(200)
+        .json({
+            success:true,
+            message:"Successfully",
+            data:tours,
+        });
+
     } catch (error) {
-        
+        res
+        .status(404)
+        .json({success:false,message:"Not found"});
     }
-}*/
+}
