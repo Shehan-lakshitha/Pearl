d2eff2efa4e4ec67e971bbd9fa2e46e56fb6b7ad
Manesha-99 <maneshatharindaofficial@gmail.com>
2023-06-28 03:09:09 +0530

    Created Header

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,9 +1,10 @@
-import React, {useRef, useEffect} from 'react'
+import React, {useRef, useEffect,useContext} from 'react'
 import { Container, Row, Button} from 'reactstrap';
-import {NavLink, Link} from 'react-router-dom';
+import {NavLink, Link,useNavigate} from 'react-router-dom';
 
 import logo from '../../assets/images/logo.png';
 import "./header.css";
+import {Authcontext} from "./../../context/AuthContext";
 
 const nav_links =[
   {
@@ -23,6 +24,13 @@ const nav_links =[
 const Header = () => {
 
   const headerRef = useRef(null);
+  const navigate = useNavigate()
+  const {user,dispatch} = useContext(Authcontext)
+
+  const logout = ()=>{
+    dispatch({type:'LOGOUT'})
+    navigate('/')
+  }
 
   const stickyHeaderFunc = () => {
     window.addEventListener('scroll', () =>{
@@ -67,6 +75,20 @@ const Header = () => {
           {/* Nav bar start */}
           <div className="nav_right d-flex align-items-center gap-4">
             <div className="nav_btns d-flex align-items-center gap-4">
+
+              {
+                user? <>
+                <h5 className='mb-0'>{user.username}</h5>
+                <Button className='btn btn_dark' onClick ={logout}>Logout</Button>
+                </> : <>
+                
+                <Button className='btn secondary_btn'>
+                  <link to = "register">Registry</link>
+                </Button>
+                
+                
+                </>
+              }
               <Button className="btn secondary__btn"><Link to='./login'>Login</Link></Button>
               <Button className="btn primary__btn"><Link to='./register'>Sign Up</Link></Button>
             </div>
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,11 +1,14 @@
-import React, { useState } from "react";
+import React, { useState,useContext } from "react";
 
 import { Container, Row, Col, Form, FormGroup, Button } from "reactstrap";
-import { Link } from "react-router-dom";
+import { Link,useNavigate } from "react-router-dom";
 import "../styles/login.css";
 
 import loginImg from "../assets/images/login.png";
 import userIcon from "../assets/images/user.png";
+
+import { AuthContext} from "./../context/AuthContext";
+import {BASE_URL} from './../utils/config';
 
 const Login = () => {
   const [credentials, setCredentials] = useState({
@@ -13,12 +16,41 @@ const Login = () => {
     password: undefined,
   });
 
+const {dispatch} = useContext(AuthContext)
+const navigate = useNavigate()
+
   const handleChange = (e) => {
     setCredentials((prev) => ({ ...prev, [e.target.id]: e.target.value }));
   };
 
-  const handleClick = (e) => {
+  const handleClick = async e => {
     e.preventDefault();
+
+    dispatch({type:'LOGIN_START'})
+
+    try {
+
+      const res = await fetch('${BASE_URL)/auth/login',{
+        method: 'post' , 
+        headers:{
+          'content-type' : 'application/json'
+        },
+        credentials:'include',
+        body: JSON.stringify(credentials)
+      })
+
+      const result= await res.json()
+      if(!res.ok) alert(result.message)
+
+      console.log(result.data);
+
+      dispatch({type:'LOGIN_SUCCESS',payload:result.data})
+      navigate('/')
+      
+    } catch (err) {
+      dispatch({type:'LOGIN_FAILURE',payload:err.message})
+      
+    }
   };
 
   return (
