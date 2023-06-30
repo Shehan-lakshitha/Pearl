7104a4d57ac22d1b7a1de78be843cced2276d8ef
Dilini Gnanarathne <shashikaladilini11@gmail.com>
2023-06-30 19:05:54 +0530

    Rewiew/tour/user

--- backend/routes/review.js
+++ backend/routes/review.js
@@ -2,5 +2,5 @@ import express from 'express'
 import { createReview } from './../controlles/reviewController.js';
 import { verifyUser } from '../utils/verifyToken.js';
 const router = express.Router();//const router=express.Router()
-router.post('/:tourId',verifyUser,createReview)
+router.post('/:tourId',/*verifyUser,*/createReview)
 export default router;
